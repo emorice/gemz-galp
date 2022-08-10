8f56a33d99fc16c582b05c704fe79372a3ccdfaa
Étienne Morice <etienne.morice@mpibpc.mpg.de>
2022-08-10 13:32:59 +0200

    First distributed fit test

--- tests/test_gemz_galp.py
+++ tests/test_gemz_galp.py
@@ -6,9 +6,13 @@ import numpy as np
 
 import pytest
 
+import galp
 import galp.graph
 
+import gemz
 from gemz_galp import models
+
+# pylint: disable=redefined-outer-name
 
 @pytest.fixture
 def data():
@@ -19,6 +23,17 @@ def data():
 
     return rng.normal(0., 1., (2, 11, 13))
 
+@pytest.fixture
+async def client():
+    """
+    Galp client connected to a forked worker
+    """
+    config = {
+        'steps': [ 'gemz_galp.models' ]
+        }
+    async with galp.local_system(**config) as client:
+        yield client
+
 def test_fit_creates_task(data):
     """
     Tests that the fit function creates a galp task instead of returning a
@@ -27,6 +42,20 @@ def test_fit_creates_task(data):
 
     train, _ = data
 
-    fitted = models.fit({'model': 'linear'}, train.tolist())
+    fitted = models.fit({'model': 'linear'}, train)
 
     assert isinstance(fitted, galp.graph.Task)
+
+async def test_run_fit(data, client):
+    """
+    Actually runs a linear fit through gemz
+    """
+    train, test = data
+    model_def = {'model': 'linear'}
+    fitted = await client.run(
+        models.fit(model_def, train)
+        )
+
+    preds = gemz.models.predict_loo(model_def, fitted, test)
+
+    assert preds.shape == test.shape
