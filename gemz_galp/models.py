5d34f68452dc81648a60705465b6c199c42e39b1
Étienne Morice <etienne.morice@mpibpc.mpg.de>
2022-08-31 17:14:15 +0200

    refactor: name changes in main module

--- gemz_galp/models.py
+++ gemz_galp/models.py
@@ -5,7 +5,7 @@ Drop-in for gemz.models with key functions wrapped in galp steps
 import sys
 from functools import partial
 
-import gemz.models as gm
+from gemz.models import ops
 
 from galp.graph import StepSet
 
@@ -14,15 +14,15 @@ export = StepSet()
 # Stpes. Wrapping could be automated, but would not be much easier to maintain
 # and would break many static analysis programs
 
-fit = export(gm.fit)
-predict_loo = export(gm.predict_loo)
-eval_loss = export(gm.eval_loss)
-fold = export(gm.fold, items=2) # -> (train, test)
-aggregate_losses = export(gm.aggregate_losses)
+fit = export(ops.fit)
+predict_loo = export(ops.predict_loo)
+eval_loss = export(ops.eval_loss)
+fold = export(ops.fold, items=2) # -> (train, test)
+aggregate_losses = export(ops.aggregate_losses)
 
 # Meta steps:
 
 _self = sys.modules[__name__]
 
-fit_eval = partial(gm.fit_eval, ops=_self)
-cv_fit_eval = partial(gm.cv_fit_eval, ops=_self)
+fit_eval = partial(ops.fit_eval, _ops=_self)
+cv_fit_eval = partial(ops.cv_fit_eval, _ops=_self)
