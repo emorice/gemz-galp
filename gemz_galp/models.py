803506aa09da582e2e797624d69e869b0b4d12e7
Étienne Morice <pro.emorice@mail.com>
2023-03-12 04:58:55 +0100

    Fix ops module being passed through galp
    The extra ops argument is a whole module, we don't want to serialize and
    deserialize it, it should be set inside wrappers and be invisible to galp.
    
    At best, passing it through galp is inefficient, at worse it simply fails as
    we've seen when using recent jax versions (0.4.6)

--- gemz_galp/models.py
+++ gemz_galp/models.py
@@ -3,9 +3,9 @@ Drop-in for gemz.models with key functions wrapped in galp steps
 """
 
 import sys
-from functools import partial
+from functools import wraps
 
-from gemz.models import ops
+from gemz.models import ops as _ops
 
 from galp.graph import Block
 
@@ -14,22 +14,38 @@ export = Block()
 # Steps. Wrapping could be automated, but would not be much easier to maintain
 # and would break many static analysis programs
 
-_fit = export(ops.fit)
-predict_loo = export(ops.predict_loo)
-eval_loss = export(ops.eval_loss)
-fold = export(ops.fold, items=3) # -> (train, test, mask)
-aggregate_losses = export(ops.aggregate_losses)
-aggregate_residuals = export(ops.aggregate_residuals)
-build_eval_grid = export(ops.build_eval_grid)
-select_best = export(ops.select_best)
-
-_cv_residualize = export(ops.cv_residualize)
+predict_loo = export(_ops.predict_loo)
+eval_loss = export(_ops.eval_loss)
+fold = export(_ops.fold, items=3) # -> (train, test, mask)
+aggregate_losses = export(_ops.aggregate_losses)
+aggregate_residuals = export(_ops.aggregate_residuals)
+select_best = export(_ops.select_best)
 
 # Meta steps:
 
 _self = sys.modules[__name__]
 
-fit = partial(_fit, _ops=_self)
-fit_eval = partial(ops.fit_eval, _ops=_self)
-cv_fit_eval = partial(ops.cv_fit_eval, _ops=_self)
-cv_residualize = partial(_cv_residualize, _ops=_self)
+# pylint: disable=missing-function-docstring
+# @wraps sets the docstring but pylint does not seem to know that
+
+@export
+@wraps(_ops.fit)
+def fit(*args, **kwargs):
+    return _ops.fit(*args, **kwargs, _ops=_self)
+
+@wraps(_ops.fit_eval)
+def fit_eval(*args, **kwargs):
+    return _ops.fit_eval(*args, **kwargs, _ops=_self)
+
+@wraps(_ops.cv_fit_eval)
+def cv_fit_eval(*args, **kwargs):
+    return _ops.cv_fit_eval(*args, **kwargs, _ops=_self)
+
+@export
+@wraps(_ops.cv_residualize)
+def cv_residualize(*args, **kwargs):
+    return _ops.cv_residualize(*args, **kwargs, _ops=_self)
+
+@wraps(_ops.build_eval_grid)
+def build_eval_grid(*args, **kwargs):
+    return _ops.build_eval_grid(*args, **kwargs, _ops=_self)
